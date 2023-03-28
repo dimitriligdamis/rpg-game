b5c87ba8f9c2cf697f36160308456188366e225b
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2023-03-29 00:19:42 +0200

    :rocket: feat: draw the board

--- js/app.js
+++ js/app.js
@@ -10,9 +10,37 @@ const app = {
     y: 3,
   },
 
+  drawBoard: function () {
+    const board = document.getElementById("board");
+
+    for (let x = 0; x < 4; x++) {
+      const newRow = document.createElement("div");
+      newRow.classList.add("row");
+      for (let y = 0; y < 6; y++) {
+        const newCell = document.createElement("div");
+        newCell.classList.add("cell");
+        newRow.appendChild(newCell);
+      }
+
+      board.appendChild(newRow);
+    }
+  },
+
   init: function () {
     console.log("init !");
+    app.drawBoard();
   },
 };
 
 document.addEventListener("DOMContentLoaded", app.init);
+
+{
+  /* <div class="row">
+<div class="cell"></div>
+<div class="cell"></div>
+<div class="cell"></div>
+<div class="cell"></div>
+<div class="cell"></div>
+<div class="cell"></div>
+</div> */
+}
