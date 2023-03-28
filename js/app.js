4e553f46f85e38a1279e49de80eea957d67a5abb
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2023-03-28 23:53:05 +0200

    :rocket: feat: define default value

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-var app = {
+const app = {
+  player: {
+    x: 0,
+    y: 0,
+    direction: "right",
+  },
+
+  targetCell: {
+    x: 5,
+    y: 3,
+  },
+
   init: function () {
-    console.log('init !');
-  }
+    console.log("init !");
+  },
 };
 
-document.addEventListener('DOMContentLoaded', app.init);
+document.addEventListener("DOMContentLoaded", app.init);
